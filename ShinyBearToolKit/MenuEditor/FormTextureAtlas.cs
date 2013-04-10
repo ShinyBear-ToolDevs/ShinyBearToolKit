ad1a5ece77532ffe922c3062642d6fc31a682300
Marfyy <martinsafsten@gmail.com>
2013-04-11 00:54:28 +0200

    Fixed the adding of images to the imagelist, so it shows the thumbnails. Fixed properties in SpriteListManager.cs for handeling of the image list. Fixed multiselect in the filedialog, and added support for it
    Signed-off-by: Marfyy <martinsafsten@gmail.com>

--- ShinyBearToolKit/MenuEditor/FormTextureAtlas.cs
+++ ShinyBearToolKit/MenuEditor/FormTextureAtlas.cs
@@ -35,16 +35,40 @@ namespace ShinyBearToolKit.MenuEditor
         private void btnImage_Click(object sender, EventArgs e)
         {
             spriteListManager.OpenImage();
+            LoadImages();
         }
 
-        public void ShowImage()
+        //public void ShowImage()
+        //{
+
+        //    for (int m = 1; m < image; m++)
+        //    {
+        //        listView1.Items.AddRange(spriteListManager.OpenImage());
+
+        //    }
+        //}
+        /// <summary>
+        /// Loads the images from SpriteListManager's List, and imputs them into the listview
+        /// </summary>
+        public void LoadImages()
         {
-
-            for (int m = 1; m < image; m++)
+            //Clears the list of old data
+            listView1.Items.Clear();
+            imageList1.Images.Clear();
+            
+            for (int m = 0; m < spriteListManager.NrOfImages; m++)
             {
-                listView1.Items.AddRange(spriteListManager.OpenImage());
-
+                //Adds the image from the spriteListManager at the specific index to the imageList
+                imageList1.Images.Add(spriteListManager.getImageAtIndex(m));
+                //Creates a temporary ListViewItem to hold the image index
+                ListViewItem item = new ListViewItem();
+                //Sets the image index
+                item.ImageIndex = m;
+                //Adds the image at the image index to the listview
+                listView1.Items.Add(item);
             }
+            //Updates
+            listView1.Update();
         }
 
        
--- ShinyBearToolKit/MenuEditor/SpriteListManager.cs
+++ ShinyBearToolKit/MenuEditor/SpriteListManager.cs
@@ -16,26 +16,54 @@ namespace ShinyBearToolkit.MenuEditor
     {
         //list with sprites.
         List<Image> image = new List<Image>();
-
+        /// <summary>
+        /// Gets or sets the images stored for the thumbnail list.
+        /// </summary>
+        public List<Image> Images
+        {
+            get { return this.image; }
+            set { this.image = value; }
+        }
+        /// <summary>
+        /// Gets the number of stored images stored for the thumbnail list.
+        /// </summary>
+        public int NrOfImages
+        {
+            get { return image.Count; }
+        }
         public void OpenImage()
         {
 
             OpenFileDialog i = new OpenFileDialog();
+            //Enables multiselect
+            i.Multiselect = true;
             i.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png;) | *.png; *.jpg; *. jpeg; *.gif; *.bmp";
 
-            if( i.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            if (i.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                    this.image.Add(Image.FromFile(i.FileName));
+                //Checks the number of images "i" contains
+                for (int m = 0; m < i.FileNames.Length; m++)
+                {
+                    //Adds each file to the image list
+                    this.image.Add(Image.FromFile(i.FileNames[m]));
+                }
 
-                    //Check the file type
-                    int dotIndex = i.FileName.LastIndexOf('.');
-                    imageExtension = i.FileName.Substring(i.FileName.Length - dotIndex);
+                //Check the file type
+                int dotIndex = i.FileName.LastIndexOf('.');
+                imageExtension = i.FileName.Substring(i.FileName.Length - dotIndex);
 
             }
         }
-        string imageExtension = "";
-
-       
+        /// <summary>
+        /// Gets the image at the specified index
+        /// </summary>
+        /// <param name="index">specified index</param>
+        /// <returns>The image at the index</returns>
+        public Image getImageAtIndex(int index)
+        {
+            return image[index];
         }
+        string imageExtension = "";
     }
+}
 
