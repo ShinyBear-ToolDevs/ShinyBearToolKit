fd1ff314fdb1b457142f45c8ba1ff574a4052a76
Johan <johanfredriksson1987@gmail.com>
2013-04-12 12:34:25 +0200

    Testing
    Signed-off-by: Johan <johanfredriksson1987@gmail.com>

--- ShinyBearToolKit/Program.cs
+++ ShinyBearToolKit/Program.cs
@@ -14,7 +14,7 @@ namespace ShinyBearToolKit
         [STAThread]
         static void Main()
         {
-            Console.WriteLine("hej");
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
