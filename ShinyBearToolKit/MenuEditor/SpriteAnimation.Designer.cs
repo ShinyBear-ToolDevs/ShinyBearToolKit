1f15445e05728313e1a67b8fcc490c4cef2478cf
Johan <johanfredriksson1987@gmail.com>
2013-04-12 20:50:48 +0200

    New AnimationForm added
    Signed-off-by: Johan <johanfredriksson1987@gmail.com>

---
+++ ShinyBearToolKit/MenuEditor/SpriteAnimation.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ShinyBearToolKit.MenuEditor
+{
+    partial class SpriteAnimation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(27, 89);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(302, 452);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemDrag += new System.Windows.Forms.ItemDragEventHandler(this.listView_ItemDrag);
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView_DragEnter);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(413, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(332, 200);
+            this.panel1.TabIndex = 2;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel_Enter);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(413, 315);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(332, 226);
+            this.panel2.TabIndex = 3;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter);
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // SpriteAnimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(784, 602);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "SpriteAnimation";
+            this.Text = "SpriteAnimation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
---
+++ ShinyBearToolKit/MenuEditor/SpriteAnimation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShinyBearToolKit.MenuEditor
+{
+    public partial class SpriteAnimation : Form
+    {
+        public SpriteAnimation()
+        {
+            InitializeComponent();
+        }
+
+        private void listView_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void listView_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void listView_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void panel_Enter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
