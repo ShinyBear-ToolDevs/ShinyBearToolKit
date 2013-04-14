6f95e0b3248c35ef63d4bb13cf559591d0623e2c
Johan <johanfredriksson1987@gmail.com>
2013-04-14 17:25:25 +0200

    Made a DataManager.cs witch will handle all the forms data.
    Signed-off-by: Johan <johanfredriksson1987@gmail.com>

---
+++ ShinyBearToolKit/Data/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShinyBearToolKit.Data
+{
+    class DataManager
+    {
+        // här ska all data lagras så att ingenting lagras i formerna.
+        // formarna ska hämta data från DataManagern
+        // All data i hella programmet ska lagras i denna klass.
+        // som det är nu lagras all data i varje enskild form, vilket är fel.
+    }
+}
